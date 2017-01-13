5aeb158f6ae6fd9fceb7003fec0e40f4e03c93df
Ben Kuper <bkuperberg@gmail.com>
2017-01-13 15:03:09 +0100

    Realtime Reflection

---
+++ Assets/TestScenes/RealtimeReflection.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class RealtimeReflection : MonoBehaviour {
+    
+    ReflectionProbe probe;
+    
+    void Awake() {
+        probe = GetComponent<ReflectionProbe>();
+    }
+    
+    void Update () {
+        probe.transform.position = new Vector3(
+            Camera.main.transform.position.x, 
+            Camera.main.transform.position.y * -1, 
+            Camera.main.transform.position.z
+        );
+
+        probe.RenderProbe();
+    }
+}
